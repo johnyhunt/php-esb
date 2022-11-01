b38720d83eb68d06013e7b8ea463e6d137fb4139
johnyhunt <evnec@opsway.com>
2022-11-01 17:04:38 +0200

    Add comments for TransportMiddleware

--- src/Middleware/Core/TransportMiddleware.php
+++ src/Middleware/Core/TransportMiddleware.php
@@ -12,6 +12,16 @@ class TransportMiddleware implements ESBMiddlewareInterface
 {
     public function process(RouteData $data, Route $route, CoreHandlerInterface $handler)
     {
+        // 1. Get DSN and detect transport sending interface
+            // 1.1 For sync interface, with use default sync transport
+            // 1.2 For async - trigger exception if not setup async transport
+
+        // 2. Authorize request (if you need)
+
+        // 3. Using DSN(Method sending) send body $data->targetRequest()->body to target system (ServerDSN->path or QueueDSN->action)
+
+        // 4. $handler->handle with withTargetResponse
+
         return $handler->handle($data->withTargetResponse(new TargetResponse('ok', 200)), $route);
     }
 }
