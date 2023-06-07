518d0cf9eec9c05e2f6c4bb2cc517794fc8432c2
Godlevsky <magod@opsway.com>
2023-06-07 14:27:19 +0300

    cs fix

--- src/ContainerConfig.php
+++ src/ContainerConfig.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ESB;
 
-use Bus\Service\BusDynamicPropertiesFetcher;
 use ESB\Auth\AuthServiceInterface;
 use ESB\Exception\SetupException;
 use ESB\Handlers\HTTP\ESBHandler;
